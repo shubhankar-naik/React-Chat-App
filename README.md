3a03d9f67c73f36e95d186f31f1bc7b3adad4bb3
Shubhankar Naik <shubhankarnaik11012000@gmail.com>
2020-05-24 08:43:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+### [Live Site](https://my-chatapp-v1.herokuapp.com/)
+
+Setup:
+- run ```npm i && npm start``` for both client and server side to start the development server
---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
+      <h2>Created by Shubhankar Naik  <span role="img" aria-label="emoji">❤️</span></h2>
+      <h2>Try it! <span role="img" aria-label="emoji">⬅️</span></h2>
+      <h4>Note:This site is not connected to a database so if you refresh the page, all the chats will be cleared out.</h4>
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>People currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
